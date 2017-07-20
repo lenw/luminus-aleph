518cb2d4c6aea680e54429bbe94986dd4adbe7dc
Len Weincier <len@cloudafrica.net>
2017-07-20 13:47:40 +0200

    Update project.clj
    Update aleph version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [aleph "0.4.2-alpha8"]])
+                 [aleph "0.4.3"]])
